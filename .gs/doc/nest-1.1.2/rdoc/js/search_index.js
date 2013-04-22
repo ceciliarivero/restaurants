5c8a6ab13a4059045dbcd5ca36e0183e2f69ea4d
Cecilia Rivero <ceciliarc@yahoo.com>
2013-04-22 20:35:33 -0300

    Delete .gs folder

--- .gs/doc/nest-1.1.2/rdoc/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var search_data = {"index":{"searchIndex":["nest","[]()","new()"],"longSearchIndex":["nest","nest#[]()","nest::new()"],"info":[["Nest","","Nest.html","",""],["[]","Nest","Nest.html#method-i-5B-5D","(key)",""],["new","Nest","Nest.html#method-c-new","(key, redis = Redis.current)",""]]}}
